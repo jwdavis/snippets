8bc30b0c54f4ec240c78bc2f4468ccde8a88c51e
Jeff Davis <jwdavis@users.noreply.github.com>
2022-07-31 09:56:29 -0700

    before flights

--- arch_w_gke.sh
+++ arch_w_gke.sh
@@ -4,8 +4,18 @@ k get deploy -w
 k get rs -w
 k apply -f nginx.yaml
 
+# rolling update
+# edit to use 1.16.1
+k describe deployment nginx-deployment
 
-kubectl rollout status deployment/nginx-deployment
-kubectl get rs
-kubectl get pods
-kubectl describe deployments
+# rollback
+# break it using 1.161
+k rollout status deployment/nginx-deployment
+k get pods
+k describe deployment nginx-deployment
+k rollout history deployment/nginx-deployment --revision=2
+k rollout undo deployment/nginx-deployment
+k describe deployment nginx-deployment
+
+# scaling a deployment
+kubectl scale deployment/nginx-deployment --replicas=10
