61941d8c0e44f7e56e8ce1b96a4671dcf517cff0
Jeff Davis <jwdavis@users.noreply.github.com>
2023-04-10 21:42:27 -0700

    t4

--- demo.sh
+++ demo.sh
@@ -5,12 +5,14 @@ k get deploy
 k get rs
 
 # show rolling update
-k apply -f ng.yaml
-k get deploy -w
-k get rs -w
-k describe deployment nginx-deployment
+k get deploy -w # 2nd shell session
+k get rs -w #3rd shell session
+k apply -f ng.yaml # 1st shell session
+k describe deployment nginx-deployment # 1st shell session
 
 # rollback
 k rollout history deployment/nginx-deployment --revision=2
 k rollout undo deployment/nginx-deployment
 k describe deployment nginx-deployment
+
+# modify service with color for blue/green
