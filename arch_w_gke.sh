363b6e3acef3e60e5d42fe05cbe2d5ba85d72a61
Jeff Davis <jwdavis@users.noreply.github.com>
2023-04-10 21:10:46 -0700

    what a mess!

--- arch_w_gke.sh
+++ arch_w_gke.sh
@@ -1,18 +1,10 @@
-# setup
-gcloud config set project jwd-gcp-demos
-gcloud config set compute/zone us-central1-a
-
-# create gke cluster
-gcloud container clusters create demos
-
 # show deployment
+k get pods -w
 k get deploy -w
 k get rs -w
-k get pods -w
 k apply -f nginx.yaml
 
 # rolling update
-# edit to use 1.16.1
 k describe deployment nginx-deployment
 
 # rollback
@@ -23,7 +15,3 @@ k describe deployment nginx-deployment
 k rollout history deployment/nginx-deployment --revision=2
 k rollout undo deployment/nginx-deployment
 k describe deployment nginx-deployment
-
-# scaling a deployment
-kubectl scale deployment/nginx-deployment --replicas=10
-
