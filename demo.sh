2d81b58a4f329053d3fe6315e3e0734e04329000
Jeff Davis <jwdavis@users.noreply.github.com>
2023-04-10 21:20:27 -0700

    tweaks

--- demo.sh
+++ demo.sh
@@ -1,17 +1,16 @@
 # show deployment
+k apply -f nb.yaml
 k get pods -w
 k get deploy -w
 k get rs -w
-k apply -f nginx.yaml
 
-# rolling update
+# show rolling update
+k apply -f ng.yaml
+k get deploy -w
+k get rs -w
 k describe deployment nginx-deployment
 
 # rollback
-# break it using 1.161
-k rollout status deployment/nginx-deployment
-k get pods
-k describe deployment nginx-deployment
 k rollout history deployment/nginx-deployment --revision=2
 k rollout undo deployment/nginx-deployment
 k describe deployment nginx-deployment
